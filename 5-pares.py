1246e414c8600b20d40fb5f9012cc5cfa4b75005
janeilson-araujo <146784212+janeilson-araujo@users.noreply.github.com>
2023-12-16 05:55:40 -0300

    Atualizar o 5-pares.py

--- 5-pares.py
+++ 5-pares.py
@@ -3,5 +3,5 @@ numpares = 0#a variavel numpares armazena a quantidade de numeros pares
 for i in range(5):#o for é utilizado para perguntar os 5 numeros 
     numeros = int(input("digite os numeros:"))
     if numeros % 2 == 0 :#o if analisa de os numeros são pares ou inpares
-        numpares = pares + 1 #se o numero for par é adicionado +1 a numpares
+        numpares = numpares + 1 #se o numero for par é adicionado +1 a numpares
 print(f"{numpares} valores pares")#exibimos a quantidade de numeros pares
